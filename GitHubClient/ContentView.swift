ceb0b41bf563f72718f4a6f3cd978ef67918cbe4
YutoFunaki <kaizuka0629@gmail.com>
2023-02-08 12:20:39 +0900

    Initial Commit

---
+++ GitHubClient/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  GitHubClient
+//
+//  Created by 船木勇斗 on 2023/02/08.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ GitHubClient/GitHubClientApp.swift
@@ -0,0 +1,17 @@
+//
+//  GitHubClientApp.swift
+//  GitHubClient
+//
+//  Created by 船木勇斗 on 2023/02/08.
+//
+
+import SwiftUI
+
+@main
+struct GitHubClientApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
