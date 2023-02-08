89552bae8ac20804b844825ab9884fd52a2a8d0e
YutoFunaki <kaizuka0629@gmail.com>
2023-02-08 12:33:05 +0900

    1-2まで終わり

--- GitHubClient/ContentView.swift
+++ GitHubClient/ContentView.swift
@@ -9,11 +9,18 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        HStack {
+            Image("github-mark")
+                .resizable()
+                .frame(width: 44.0,height: 44.0)
+            VStack(alignment: .leading){
+                Text("Owner Name")
+                    .font(.caption)
+                Text("Repository Name")
+                    .font(.body)
+                    .fontWeight(.semibold)
+            }
+            
         }
         .padding()
     }
